f2871719fef8137f3ed0d81019afb93e82a6bc2e
mauricio9002 <mauricio.golemba@escola.pr.gov.br>
2025-10-22 09:44:14 -0300

    Add files via upload

---
+++ kk/criar_lista.js
@@ -0,0 +1,8 @@
+// container
+const container = document.createElement('div');
+container.id = 'lista-container';
+document.body.appendChild(container);
+
+container.style.backgroundColor = '#f9f9f9';
+container.style.border = '2px solid #ccc';
+container.style.borderrabius = 
